48028a105bb64d187a54fd008a3aa7300b89d0f8
ricardoslv <ricardoslv80@gmail.com>
2020-08-26 22:43:54 +0100

    Isolate cron starters, add explicit information on clock running channels, manage clock start and stop

--- src/bot.js
+++ src/bot.js
@@ -5,6 +5,31 @@ const { Client } = require('discord.js')
 const ytdl = require('ytdl-core');
 const cron = require('node-cron');
 
+const hourNotice = (channel,voiceChannel) => 
+    cron.schedule('0 * * * *', () => {
+        const currHour = new Date().getHours()
+        channel.send(`It's ${currHour} o'clock 🕒`)
+        if (voiceChannel) {
+            playBell(voiceChannel, currHour % 12)
+        }
+    })
+
+const halfHourNotice = (channel,voiceChannel) => 
+        cron.schedule('30 * * * *', () => {
+        const currHour = new Date().getHours()
+        channel.send(`It's ${currHour} o'clock 🕒`)
+        if (voiceChannel) {
+            playBell(voiceChannel, currHour % 12)
+        }
+    })
+
+const status = {
+    clockNotice: false,
+    clockChannels: [null,null],
+    hourNotice: null,
+    halfHourNotice: null
+}
+
 async function playYoutube(channel, link) {
 
     const connection = await channel.join();
@@ -12,20 +37,16 @@ async function playYoutube(channel, link) {
         .on('finish', () => setTimeout(() => { channel.leave() }, 500))
 }
 
-async function playBell(channel,rings){
-    if(rings===0){
+async function playBell(channel, rings) {
+    if (rings === 0) {
         setTimeout(() => { channel.leave() }, 500)
     }
-    else{
+    else {
         const connection = await channel.join();
-        connection.play(ytdl('https://www.youtube.com/watch?v=dNl4-w9ZrBs', { filter: 'audioonly' }))
-        .on('finish', () => playBell(channel,rings-1))
+        connection.play(ytdl('https://www.youtube.com/watch?v=dNl4-w9ZrBs', { filter: 'audioonly', volume: 0.35 }))
+            .on('finish', () => playBell(channel, rings - 1))
     }
 }
-
-cron.schedule('0 * * * *', () => {
-    console.log('running a task every minute');
-});
 
 const client = new Client()
 
@@ -41,33 +62,38 @@ client.on('message', async (message) => {
 
     console.log(`${message.author.tag} said: ${message.content}`)
 
-    // if (message.author.tag === 'Faria#9688') {
-    //     message.reply('Faria stfu')
-    //     return
-    // }
-
     if (message.content === 'BobBot say hi') {
         message.reply('Wtf is up!!')
         message.channel.send('I sleep now')
     }
 
-    if(message.content==='bob count the hours'){
-        const channel = message.channel
+    if (message.content === 'bob start the clock') {
         const voiceChannel = message.member.voice.channel
-        channel.send('Starting the time 🕒')
-        cron.schedule('0 * * * *', () => {
-            const currHour = new Date().getHours()
-            channel.send(`It's ${currHour} o'clock 🕒`)
-            if(voiceChannel){
-                playBell(voiceChannel,currHour)
-            }
-        });
-        cron.schedule('30 * * * *', () => {
-            channel.send(`It's half past ${new Date().getHours()} o'clock 🕒`)
-            if(voiceChannel){
-                playBell(voiceChannel,2)
-            }
-        });
+        
+        if (status.clockNotice === true) {
+            message.reply(`There's already a clock 🕒 running on Text: ${status.clockChannels[0]} and Voice: ${status.clockChannels[1]}`)
+        }
+        else {
+            status.clockChannels=[message.channel,voiceChannel]
+            message.channel.send(`Starting the clock 🕒 on Text: ${message.channel} and Voice: ${voiceChannel}`)
+            status.clockNotice = true;
+            status.hourNotice = hourNotice(message.channel,voiceChannel)
+            status.halfHourNotice = halfHourNotice(message.channel,voiceChannel)
+        }
+
+    }
+
+    if (message.content === 'bob stop the clock') {
+        
+        if (status.clockNotice === false) {
+            message.reply("There's no clock 🕒 running")
+        }
+        else {
+            message.channel.send('Die clock 🕒')
+            status.clockNotice = false;
+            status.hourNotice.destroy()
+            status.halfHourNotice.destroy()
+        }
     }
 
     const voiceChannel = message.member.voice.channel
@@ -84,12 +110,12 @@ client.on('message', async (message) => {
                 case 'bruh':
                     playYoutube(voiceChannel, 'https://www.youtube.com/watch?v=2ZIpFytCSVc')
                     break;
-                case 'die': 
+                case 'die':
                     playYoutube(voiceChannel, 'https://www.youtube.com/watch?v=PtcHNvVooLQ')
                     break
                 case 'bell':
-                    playBell(voiceChannel,msgTokens[2])
-                    break  
+                    playBell(voiceChannel, msgTokens[2])
+                    break
                 case 'belldie':
                     playYoutube(voiceChannel, 'https://www.youtube.com/watch?v=zDBrQWq82-Q')
                     break
