75a085ac942df8786249926338d501e1330d023a
ricardoslv <ricardoslv80@gmail.com>
2020-08-26 19:35:20 +0100

    Add cron job trigger to tell the time on text and voice

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const { Client } = require('discord.js')
 const ytdl = require('ytdl-core');
+const cron = require('node-cron');
 
 async function playYoutube(channel, link) {
 
@@ -10,6 +11,21 @@ async function playYoutube(channel, link) {
     connection.play(ytdl(link, { filter: 'audioonly' }))
         .on('finish', () => setTimeout(() => { channel.leave() }, 500))
 }
+
+async function playBell(channel,rings){
+    if(rings===0){
+        setTimeout(() => { channel.leave() }, 500)
+    }
+    else{
+        const connection = await channel.join();
+        connection.play(ytdl('https://www.youtube.com/watch?v=dNl4-w9ZrBs', { filter: 'audioonly' }))
+        .on('finish', () => playBell(channel,rings-1))
+    }
+}
+
+cron.schedule('0 * * * *', () => {
+    console.log('running a task every minute');
+});
 
 const client = new Client()
 
@@ -25,14 +41,33 @@ client.on('message', async (message) => {
 
     console.log(`${message.author.tag} said: ${message.content}`)
 
-    if (message.author.tag === 'Faria#9688') {
-        message.reply('Faria stfu')
-        return
-    }
+    // if (message.author.tag === 'Faria#9688') {
+    //     message.reply('Faria stfu')
+    //     return
+    // }
 
     if (message.content === 'BobBot say hi') {
         message.reply('Wtf is up!!')
         message.channel.send('I sleep now')
+    }
+
+    if(message.content==='bob count the hours'){
+        const channel = message.channel
+        const voiceChannel = message.member.voice.channel
+        channel.send('Starting the time 🕒')
+        cron.schedule('0 * * * *', () => {
+            const currHour = new Date().getHours()
+            channel.send(`It's ${currHour} o'clock 🕒`)
+            if(voiceChannel){
+                playBell(voiceChannel,currHour)
+            }
+        });
+        cron.schedule('30 * * * *', () => {
+            channel.send(`It's half past ${new Date().getHours()} o'clock 🕒`)
+            if(voiceChannel){
+                playBell(voiceChannel,2)
+            }
+        });
     }
 
     const voiceChannel = message.member.voice.channel
@@ -42,18 +77,25 @@ client.on('message', async (message) => {
             switch (msgTokens[1]) {
                 case 'gong':
                     playYoutube(voiceChannel, 'https://www.youtube.com/watch?v=r7oAsDWy6n4')
-                    break;
+                    break
                 case 'chingchong':
                     playYoutube(voiceChannel, 'https://www.youtube.com/watch?v=8yKG9VncnBI')
-                    break;
+                    break
                 case 'bruh':
                     playYoutube(voiceChannel, 'https://www.youtube.com/watch?v=2ZIpFytCSVc')
                     break;
                 case 'die': 
-                    playYoutube(voiceChannel, 'https://www.youtube.com/watch?v=PtcHNvVooLQ') 
+                    playYoutube(voiceChannel, 'https://www.youtube.com/watch?v=PtcHNvVooLQ')
+                    break
+                case 'bell':
+                    playBell(voiceChannel,msgTokens[2])
+                    break  
+                case 'belldie':
+                    playYoutube(voiceChannel, 'https://www.youtube.com/watch?v=zDBrQWq82-Q')
+                    break
             }
         } else {
-            message.reply('You need to join a voice channel first!');
+            message.reply('You need to join a voice channel first!')
         }
     }
 })
